af948cd0f9d020129109b3533c4a39288e1298f9
刘云 <liuyun@liuyundeMac-mini.local>
2022-08-20 17:35:22 +0800

    remove dd ; fix bug

--- config/queue.php
+++ config/queue.php
@@ -38,7 +38,7 @@ return [
             'driver' => 'database',
             'table' => 'jobs',
             'queue' => 'default',
-            'retry_after' => 9000000,
+            'retry_after' =>  600,
             'after_commit' => false,
         ],
 
