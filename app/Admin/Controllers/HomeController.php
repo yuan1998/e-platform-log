cf106d8738183d5b6abb237c2a1c1123712ca8c7
yuan1998 <chizhiyueshu@gmail.com>
2023-05-08 09:23:38 +0800

    优化,还是优化

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Admin\Controllers;
 
 use App\Admin\Metrics\Examples;
 use App\Http\Controllers\Controller;
+use Dcat\Admin\Admin;
 use Dcat\Admin\Http\Controllers\Dashboard;
 use Dcat\Admin\Layout\Column;
 use Dcat\Admin\Layout\Content;
@@ -30,8 +31,14 @@ class HomeController extends Controller
             ->description('Description...')
             ->body(function (Row $row) {
                 $row->column(6, function (Column $column) {
-                    $column->row(Dashboard::title());
-                    $column->row(new Examples\Tickets());
+                    $params = [
+                        'greeting' =>greetOfNowCn(),
+                        'username' => Admin::user()->name,
+                        'avatar' => Admin::user()->getAvatar(),
+                    ];
+                    $title = view('admin::dashboard.title',$params);
+                    $column->row($title);
+//                    $column->row(new Examples\Tickets());
                 });
 
                 $row->column(6, function (Column $column) {
