fc100caddb709f66e4e2f597581df0375853030f
yuan1998 <chizhiyueshu@gmail.com>
2023-05-08 17:26:49 +0800

    优化,还是优化

--- app/Clients/DaZhongClient.php
+++ app/Clients/DaZhongClient.php
@@ -23,7 +23,7 @@ class DaZhongClient extends BaseClient
         'fspop=test; cy=17; cye=xian; _lxsdk_cuid=187f8febf27c8-09905137b96a92-11473c11-384000-187f8febf27c8; _lxsdk=187f8febf27c8-09905137b96a92-11473c11-384000-187f8febf27c8; _hc.v=4a2683d1-9542-a371-23fc-8079a1215479.1683509658; WEBDFPID=u96y28x0uv2753x301zyz0x014z9569u812u1uvx63y979582zw6w04v-1998869658533-1683509657627IEKMOUO75613c134b6a252faa6802015be905511028; ctu=8d15db6b483921427b158623a06295426d352a7af758dcd980f8c3df64fc00d4; s_ViewType=10; qruuid=70dedc59-1284-441d-93aa-7a31ed04a8e2; dplet=456a1f4c4218e14d28a6bf1ca109ebab; dper=bc8ad98989cf34e0f4434b13b242b7feb8458c5a2fe1e0d81cad7c71fc73d06c8821a838633954b7c0569f3121af8da7c36f0d9d64edaa02ddb09c2e8383816d; ll=7fd06e815b796be3df069dec7836c3df; ua=%E7%B4%A0%E8%B4%A8%E5%A5%B3%E5%AD%A9; _lxsdk_s=187f8febf28-455-8ab-631%7C%7C41',
         '_hc.v=875208b5-2930-7808-4d4c-d9f669c627ea.1681205216; _lxsdk=1811901b903c8-04038c9409d822-11473c11-384000-1811901b903c8; _lxsdk_cuid=1811901b903c8-04038c9409d822-11473c11-384000-1811901b903c8; _lxsdk_s=1877462ae98-fd1-5f-17d%7C%7C1; aburl=1; cy=4; cye=guangzhou; dper=fec56066b8820f35d610008edcbe2a4f41bb24466fedd7ef5dd9b4a1015c6d4d4eca85cbad17d1388ff5f27350c119d39f0983f3e6874ad0d9b28562dd57d6a1; fspop=test; ll=7fd06e815b796be3df069dec7836c3df; m_flash2=1; pvhistory="6L+U5ZuePjo8L3N1Z2dlc3QvZ2V0SnNvbkRhdGE/ZGV2aWNlX3N5c3RlbT1BTkRST0lEJnlvZGFSZWFkeT1oNT46PDE2ODEyMDU3NTQ1NTZdX1s="; qruuid=630359ed-9527-4d9b-9a24-0c7d0899da17; WEBDFPID=yux665w8259w5wu6yv6zv081y8x990328123uz6x8xx979581301767v-1996565215624-1681205215624AOMUCQU75613c134b6a252faa6802015be905513004',
         '_hc.v=f0839e32-4a44-c313-1e20-e8465c700a07.1657518918; Hm_lvt_602b80cf8079ae6591966cc70a3940e7=1679106314,1680928824; cy=17; cye=xian; WEBDFPID=z8z5x3y191445uyy0w6669yw2y28vzv8813u71175w89795802v6y944-1994466257075-1679106255887CSQCEQG10f02007e9804b0b4cf483cebf1f9f512538; qruuid=704aea2c-231e-43f1-8bb1-38bc232d99fb; dper=28363b96954b191de7c8ed3e4a17d0d8527d153f86bc188a93c0c742f1ac23c56f25826efcb5af0f7a4efc695c653c242ad9dbf59d844754a6d3a4961d137239; ll=7fd06e815b796be3df069dec7836c3df; Hm_lpvt_602b80cf8079ae6591966cc70a3940e7=1681292483; fspop=test; yDCV98W0pO17F2Us5u0QDxVvXf7Jxkvr%2BYIkblPXJGg%3D="leod2VSGCsg7haVWx38F4Q=="; _lxsdk_s=18774a300aa-2d1-6fd-8e4%7C%7C27; _lx_utm=utm_source%3DBaidu%26utm_medium%3Dorganic',
-        "fspop=test; cy=17; cye=xian; _lxsdk_cuid=187f8febf27c8-09905137b96a92-11473c11-384000-187f8febf27c8; _lxsdk=187f8febf27c8-09905137b96a92-11473c11-384000-187f8febf27c8; _hc.v=4a2683d1-9542-a371-23fc-8079a1215479.1683509658; WEBDFPID=u96y28x0uv2753x301zyz0x014z9569u812u1uvx63y979582zw6w04v-1998869658533-1683509657627IEKMOUO75613c134b6a252faa6802015be905511028; dper=fec56066b8820f35d610008edcbe2a4fa34db5070416a104eb516ff69d7192ac1640e25aa19dc8805b354a0713fc1d2e4f4f47b0f05ed79e0382d348cd10a079; _lxsdk_s=187f8febf28-455-8ab-631%7C%7C25; qruuid=bbeef268-1491-41f0-ad97-ba93400aca54; ll=7fd06e815b796be3df069dec7836c3df",
+        "fspop=test; cy=17; cye=xian; _lxsdk_cuid=187f8febf27c8-09905137b96a92-11473c11-384000-187f8febf27c8; _lxsdk=187f8febf27c8-09905137b96a92-11473c11-384000-187f8febf27c8; _hc.v=4a2683d1-9542-a371-23fc-8079a1215479.1683509658; WEBDFPID=u96y28x0uv2753x301zyz0x014z9569u812u1uvx63y979582zw6w04v-1998869658533-1683509657627IEKMOUO75613c134b6a252faa6802015be905511028; dper=fec56066b8820f35d610008edcbe2a4fa34db5070416a104eb516ff69d7192ac1640e25aa19dc8805b354a0713fc1d2e4f4f47b0f05ed79e0382d348cd10a079; _lxsdk_s=187f8febf28-455-8ab-631%7C%7C25; qruuid=bbeef268-1491-41f0-ad97-ba93400aca54; ll=7fd06e815b796be3df069dec7836c3df", "dper=2145cb0381224204992a091a76ad9f8f1516e89aa08aea955c336d30d1e0d25e60b43f295341cafb85f0ba1c9dfa6504083e3ecee94889825d2d6a44a91db2ce; ll=7fd06e815b796be3df069dec7836c3df; _lxsdk_s=187faaaa577-ab8-ea3-859%7C%7C166",
     ];
 
     public static function getCookie()
@@ -44,9 +44,7 @@ class DaZhongClient extends BaseClient
     {
         $curl = curl_init();
         $cookie = self::getCookie();
-        $ua = UserAgent::random([
-            'device_type' => 'Desktop',
-        ]);
+
         $options = [
             CURLOPT_URL => 'https://mapi.dianping.com/dzbook/prepayproductdetail.json2?' . $query,
             CURLOPT_RETURNTRANSFER => true,
@@ -60,7 +58,7 @@ class DaZhongClient extends BaseClient
                 'Connection: keep-alive',
                 'sec-ch-ua: "Chromium";v="94", "Microsoft Edge";v="94", ";Not A Brand";v="99"',
                 'sec-ch-ua-mobile: ?0',
-                "User-Agent: $ua",
+                "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31",
                 'sec-ch-ua-platform: "macOS"',
                 'Accept: */*',
                 'Origin: https://www.dianping.com',
@@ -121,6 +119,7 @@ class DaZhongClient extends BaseClient
                     'content' => $result ?? $content,
                 ]);
                 self::switchCookie();
+                sleep(rand(5,10));
                 $retryCount--;
             } else {
                 Log::info('>>>大众.getProductDetailApi.OK');
@@ -292,9 +291,6 @@ class DaZhongClient extends BaseClient
                     if ($proxy = ProxyClient::getProxy())
                         $config['proxy'] = "http://$proxy";
                 $config['headers']['Cookie'] = self::getCookie();
-                $config['headers']['User-Agent'] = UserAgent::random([
-                    'device_type' => 'Desktop',
-                ]);
                 $response = $this->get($this->hospital->dz_url, $config);
                 $body = $response->getBody()->getContents();
             } catch (\Exception $exception) {
@@ -314,6 +310,7 @@ class DaZhongClient extends BaseClient
                     $proxy = null;
                 }
                 $retryCount--;
+                sleep(rand(5,10));
             } else {
                 return $body;
             }
